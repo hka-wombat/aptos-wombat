d15f1b53af93583ba9bd0a71577cb8d2a9eeb68a
HKA <141198731+hka-wombat@users.noreply.github.com>
2024-04-27 12:29:07 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,3 +7,8 @@ const transaction = await aptos.transferCoinTransaction({
   recipient: bob.accountAddress,
   amount: 100,
 })
+
+const pendingTransaction = await aptos.signAndSubmitTransaction({
+  signer: alice,
+  transaction,
+});
