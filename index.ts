1bb9a59ff4e32083e81ce569b3d9c0c75199cbd6
HKA <141198731+hka-wombat@users.noreply.github.com>
2024-05-03 11:07:52 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -42,3 +42,9 @@ const pendingTransaction = await aptos.transaction.submit.simple({
   transaction,
   senderAuthenticator,
 });
+
+// using signAndSubmit combined
+const pendingTransaction = await aptos.signAndSubmitTransaction({
+  signer: alice,
+  transaction,
+});
